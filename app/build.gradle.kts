93412dfff2b76a31d662170a40a01295082bbfd2
mjdenham <mjdenham@gmail.com>
2024-08-13 17:33:05 +0100

    Add movie image and improve movie list layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.lifecycle)
+    implementation(libs.coil.compose)
 
     implementation(libs.retrofit)
     implementation(libs.kotlinx.serialization.json)
--- app/src/main/java/com/mjdenham/movies/ui/toprated/TopRatedScreen.kt
+++ app/src/main/java/com/mjdenham/movies/ui/toprated/TopRatedScreen.kt
@@ -1,18 +1,27 @@
 package com.mjdenham.movies.ui.toprated
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
 import com.mjdenham.movies.domain.TopRatedSummaryDto
 import com.mjdenham.movies.ui.theme.MoviesTheme
 
@@ -24,21 +33,28 @@ fun TopRatedScreen(modifier: Modifier = Modifier, viewModel: TopRatedViewModel =
 
 @Composable
 private fun TopRated(movies: List<TopRatedSummaryDto.MovieDto>, modifier: Modifier) {
-    LazyColumn {
+    LazyColumn(modifier = modifier) {
         items(movies) {
-            TopRatedMovie(it, modifier)
+            TopRatedMovie(it, modifier.padding(0.dp))
         }
     }
 }
 
 @Composable
 private fun TopRatedMovie(it: TopRatedSummaryDto.MovieDto, modifier: Modifier) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-    ) {
-        Column {
-            Text(text = it.name)
-            Text(text = "Score: ${it.voteAverage}")
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        AsyncImage(
+            model = it.smallPosterPath,
+            contentDescription = "Movie poster",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .padding(15.dp)
+                .size(80.dp)
+                .clip(CircleShape)
+        )
+        Column(verticalArrangement = Arrangement.spacedBy(10.dp)) {
+            Text(text = it.name, style = MaterialTheme.typography.titleMedium)
+            Text(text = "Score: ${it.voteAverage}", style = MaterialTheme.typography.bodyMedium)
         }
     }
     HorizontalDivider()
